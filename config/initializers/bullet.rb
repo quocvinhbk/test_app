2d92f103e435a9ecc0ea5628df8a16a573897d66
quocvinhbk <quocvinhbk@gmail.com>
2018-08-25 11:26:45 +0700

    done caching

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,11 @@
+if defined? Bullet
+  Bullet.enable = true
+  # Bullet.alert = true
+  Bullet.bullet_logger = true
+end
+
+if defined? Bullet
+  Bullet.enable = true
+  Bullet.alert = true
+  # Bullet.bullet_logger = true
+end
